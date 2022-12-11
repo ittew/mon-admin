3e02ba5d90f7bab866ca9dfedf539f04e33668a6
ittew <873077939@qq.com>
2022-12-11 23:39:11 +0800

    feat: 新增layout页面布局及公共scss文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
-    'vue/multi-word-component-names': [
-      'error',
-      {
-        ignores: ['index']
-      }
-    ]
+    'vue/multi-word-component-names': 'off'
   }
 }
